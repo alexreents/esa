0f5e66155a09af3d23d3e8a9974633227f780005
Alex Reents <alexreents@Alexs-MacBook-Pro.local>
2020-09-11 16:17:04 -0700

    change to prod

--- server/routes/views/app.js
+++ server/routes/views/app.js
@@ -10,7 +10,7 @@ import webpackMiddleware from "webpack-dev-middleware";
 import webpackHotMiddleware from "webpack-hot-middleware";
 import config from "../../../webpack/webpack.config.babel";
 
-const env = process.env.NODE_ENV || "development";
+const env = process.env.NODE_ENV || "production";
 
 const routes = (app) => {
   if (env === "development") {
